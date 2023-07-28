636b129845deece6a88bef54224dc423f694ee78
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-28 17:33:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 function nonRepeatedchar(str) {
-	for(let i=1;i<str.length();i++){
-		if(i==str.length()-1&&str.charAt(i)!==str.charAt(i-1))return str.charAt(i)
+	let n=str.length;
+	for(let i=1;i<n;i++){
+		if(i===n-1&&str.charAt(i)!==str.charAt(i-1))return str.charAt(i)
        if(str[i]!==str[i-1]&&str[i]!==str[i+1])return str[i];
 	}
 }
