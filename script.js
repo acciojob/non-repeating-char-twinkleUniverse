198bb19039ce0cd701c8d80b4728f1278d7f4521
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-28 17:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function nonRepeatedchar(str) {
+	for(let i=1;i<str.length();i++){
+		if(i==str.length()-1&&str.charAt(i)!==str.charAt(i-1))return str.charAt(i)
+       if(str[i]!==str[i-1]&&str[i]!==str[i+1])return str[i];
+	}
+}
+let str="anmenn"
+console.log(nonRepeatedchar(str))
